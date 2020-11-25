82d8b6bcebe21001eea7271db800f46d1097c7c9
Salim Ahmed Ali <salim7ali@gmail.com>
2020-11-25 23:36:49 +0530

    Working, but isn't handling small cases(eg: 27)

---
+++ 91. Decode Ways/decode_ways.cpp
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/decode-ways/
+
+class Solution {
+    // int count = 0;
+public:
+    int numDecodings(string s) {
+        int count=0;
+        countDecodings(s, 0, count);
+        return count;
+    }
+    
+    bool isValidNum(string sub){
+        int num = stoi(sub);
+        
+        if(1<=num && num<=26)
+            return true;
+        else
+            return false;
+    }
+    
+    void countDecodings(string &mainStr, int i, int &count){
+        if(i>=mainStr.length()){
+            count += 1;
+            return;
+        }
+        countDecodings(mainStr, i+1, count);
+        
+        if(isValidNum(mainStr.substr(i, i+1)) && i!=mainStr.length()-1){
+            countDecodings(mainStr, i+2, count);
+        }
+            
+    }
+    
+    
+};
