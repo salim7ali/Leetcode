4536be84bfa80e9cc7456c6dc0378dc733a2ec49
Salim <salim7ali@gmail.com>
2021-02-13 22:14:05 +0530

    Seems like brackets were causing an issue. Not sure why.

--- 295. Find Median from Data Stream/295_Find_Median_from_Data_Stream.cpp
+++ 295. Find Median from Data Stream/295_Find_Median_from_Data_Stream.cpp
@@ -14,29 +14,36 @@ public:
     void addNum(int num) {
         int temp;
         
-        if(this->maxCount==0 && this->minCount==0){// when heaps are empty
+        if((this->maxCount)==0 && (this->minCount)==0){// when heaps are empty
+            cout<<"(1)\n";
             this->maxH.push(num);
             this->maxCount += 1;            
-        }else if(num< this->maxH.top()){// wants to go left into maxHeap
+        }else if(num< (this->maxH.top())){// wants to go left into maxHeap
             if(this->maxCount == (this->minCount)+1){
+                cout<<"(2)\n";
                 temp = this->maxH.top();
                 this->maxH.pop();
                 this->minH.push(temp);
                 this->minCount += 1;
                 this->maxH.push(num);
             }else{//this->maxCount == this->minCount
+                cout<<"(3)\n";
                 this->maxH.push(num);
                 this->maxCount += 1;            
             }
         }else if(this->maxH.top()< num && num<this->minH.top()){// wants to stay in the middle
             if(this->maxCount == (this->minCount)+1){
+                cout<<"(4)\n";
                 this->minH.push(num);
                 this->minCount += 1;            
             }else{//this->maxCount == this->minCount
+                cout<<"(5)\n";
                 this->maxH.push(num);
                 this->maxCount += 1;            
             }
         }else if(num> this->minH.top()){// wants to go right into minHeap
+                cout<<"(6)\n";
+
             if(this->maxCount == (this->minCount)+1){
                 this->minH.push(num);
                 this->minCount += 1;            
@@ -54,10 +61,10 @@ public:
         double median;
 
         if(this->maxCount == this->minCount){
-            median = (this->maxH.top() + this->minH.top())/2;
-            cout<<median<<" ";
+            median = ((double)this->maxH.top() + (double)this->minH.top())/2;
+            // cout<<median<<" ";
         }else{
-            median = this->maxH.top();
+            median = (double)this->maxH.top();
         }
         return median;
     }
@@ -69,3 +76,31 @@ public:
  * obj->addNum(num);
  * double param_2 = obj->findMedian();
  */
+
+int main(){
+    MedianFinder obj;
+    // obj.addNum(1);
+    // obj.addNum(1);
+    // cout<<obj.findMedian()<<"\n";
+    // obj.addNum(1);
+    // cout<<obj.findMedian();
+
+    obj.addNum(9);
+    cout<<obj.findMedian()<<"=median\n";
+    obj.addNum(2);
+    cout<<obj.findMedian()<<"=median\n";
+    obj.addNum(3);
+    cout<<obj.findMedian()<<"=median\n";
+    obj.addNum(6);
+    cout<<obj.findMedian()<<"=median\n";
+    obj.addNum(7);
+    cout<<obj.findMedian()<<"=median\n";
+    obj.addNum(8);
+    cout<<obj.findMedian()<<"=median\n";
+    obj.addNum(1);
+    cout<<obj.findMedian()<<"=median\n";
+    obj.addNum(5);
+    cout<<obj.findMedian()<<"=median\n";
+    obj.addNum(4);
+    cout<<obj.findMedian()<<"=median\n";
+}
