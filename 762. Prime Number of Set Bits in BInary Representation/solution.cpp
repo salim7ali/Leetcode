483e6715a3218b6f79c891903d52d57640cf6272
Salim <salim7ali@gmail.com>
2021-03-13 14:13:22 +0530

    optimization worked. Reduced 4ms in runtime

--- 762. Prime Number of Set Bits in BInary Representation/solution.cpp
+++ 762. Prime Number of Set Bits in BInary Representation/solution.cpp
@@ -3,6 +3,11 @@ using namespace std;
 
 class Solution {
 public:
+    Solution(){
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+    }
+
     bool hasPrimeSetBits(int num){
         int count=0;
         while(num){
@@ -13,17 +18,12 @@ public:
         return ((count==2)||(count==3)||(count==5)||(count==7)||(count==11)||
         (count==13)||(count==17)||(count==19));
     }
-
+    
     int countPrimeSetBits(int L, int R) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        
         int resCount = 0;
-
-        for(int i=L; i<=R; i++){
+        for(int i=L; i<=R; i++)
             if(hasPrimeSetBits(i))
                 resCount += 1; 
-        }
         return resCount;
     }
 };
