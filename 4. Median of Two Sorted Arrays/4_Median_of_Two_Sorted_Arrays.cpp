ee458a9fc2fdd16c72a16a47810afc1ceda340a5
Salim <salim7ali@gmail.com>
2021-04-25 23:13:59 +0530

    non optimized code

--- 4. Median of Two Sorted Arrays/4_Median_of_Two_Sorted_Arrays.cpp
+++ 4. Median of Two Sorted Arrays/4_Median_of_Two_Sorted_Arrays.cpp
@@ -1,6 +1,49 @@
+// https://leetcode.com/problems/median-of-two-sorted-arrays/
 class Solution {
 public:
+    // int findMid(){
+    //     while(left<=right){
+    //         if(singleArr){
+    //             mid = left+floor((right-left)/2);
+    //         }
+
+    //     }
+
+    // }
+
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        
+        // bool singleArr=false;
+        // int left=0, right=nums2.size()-1, mid=floor((right-left)/2);
+
+        int resultArrSize= nums1.size()+nums2.size();
+        int halfArrSize=resultArrSize/2;
+        int l1, l2, r1, r2;
+        for(r1=0; r1<nums1.size(); r1++){
+            l1=r1-1;
+            l2=halfArrSize-r1;
+            r2=l2+1;
+
+            if(l2<r1 && l1<r2){
+                return avg(max(l1, l2), max(r1, r2));
+            }
+            // }else if(l2>=r1){
+                
+            // }else{// l1>=r2
+
+            // }  
+        }
+
+        // while(1){
+        //     mid = findMid(left, right, singleArr);
+
+        // }
+
     }
 };
+
+int main(){
+    vector<int> nums1 ={1, 5, 11, 12, 15, 20, 51, 99};
+    vector<int> nums2 ={9, 16, 17, 26, 57};
+    Solution obj;
+    cout<<obj.findMedianSortedArrays(nums1, nums2)<<"\n";
+}
