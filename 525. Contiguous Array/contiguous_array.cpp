1a9b45ff1c55fd9f4030533f3d8e31f94bb357d9
Salim <salim7ali@gmail.com>
2021-05-12 00:42:45 +0530

    completed class method

--- 525. Contiguous Array/contiguous_array.cpp
+++ 525. Contiguous Array/contiguous_array.cpp
@@ -3,6 +3,35 @@
 class Solution {
 public:
     int findMaxLength(vector<int>& nums) {
-        
+        vector<int> prefixSum(nums.size(), 0);
+        unordered_map<int, vector<int>> hash_map;
+
+        int currentSum=0;
+        // forward pass
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]==0)
+                currentSum -= 1;
+            else  // nums[i] == 1
+                currentSum += 1;
+            prefixSum[i] = currentSum;
+
+            if( hash_map.find(prefixSum[i]) == hash_map.end()) // first encounter of prefixSum[i]
+                hash_map[prefixSum[i]].push_back(i);
+            // else if(hash_map[prefixSum[i]].size() == 1) // second encounter of prefixSum[i]
+        }
+
+        // backward pass
+        for(int i=nums.size()-1; i>=0; i--){
+            if(hash_map[prefixSum[i]].size() == 1)
+                 hash_map[prefixSum[i]].push_back(i);
+        }
+
+        int max_subarray = 0;
+        for(auto ele: hash_map){
+            if(ele[1] - ele[0] > max_subarray)
+                max_subarray = ele[1] - ele[0];
+        }
+
+        return max_subarray;
     }
 };
