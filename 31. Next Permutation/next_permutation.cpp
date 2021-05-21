2d97939f5aebffe6e1a49d2480942351a2e608d5
Salim <salim7ali@gmail.com>
2021-05-21 10:00:35 +0530

    first commit

---
+++ 31. Next Permutation/next_permutation.cpp
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/next-permutation/
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        
+    }
+};
--- 4. Median of Two Sorted Arrays/4_Median_of_Two_Sorted_Arrays.cpp
+++ 4. Median of Two Sorted Arrays/4_Median_of_Two_Sorted_Arrays.cpp
@@ -23,9 +23,13 @@ public:
         int l1, l2, r1, r2;
         for(int pivot=0; pivot<nums1.size(); pivot++){
             l1=nums1[pivot-1];
+            if(pivot==0)
+                l1=INT_MIN;
             r1=nums1[pivot];
-            l2=nums2[halfArrSize-pivot];
-            r2=nums2[halfArrSize-pivot+1];
+            l2=nums2[halfArrSize-pivot-2];
+            r2=nums2[halfArrSize-pivot-1];
+            if(halfArrSize-pivot-1 == nums2.size())
+                r2=INT_MAX;
 
             if(l2<r1 && l1<r2){
                 return (max(l1, l2) + max(r1, r2))/2;
@@ -47,7 +51,7 @@ public:
 
 int main(){
     vector<int> nums1 ={1, 5, 11, 12, 15, 20, 51, 99};
-    vector<int> nums2 ={9, 16, 17, 26, 57};
+    vector<int> nums2 ={9, 16, 17, 26};
     Solution obj;
     cout<<obj.findMedianSortedArrays(nums1, nums2)<<"\n";
 }
