6f5089ce529feff08e6cdc8cc507f9d0f816b7c1
Salim <salim7ali@gmail.com>
2021-05-22 17:05:52 +0530

    bug issues exist. Causing infinite loops

--- 42. Trapping Rain Water/42_Trapping_Rain_Water.cpp
+++ 42. Trapping Rain Water/42_Trapping_Rain_Water.cpp
@@ -6,6 +6,7 @@ class Solution {
     // <height, index>
     vector<pair<int, int>> left_to_right_inc_peaks;
     vector<pair<int, int>> right_to_left_inc_peaks;
+    vector<int> water_level_list;
 public:
     void display(vector<pair<int, int>> vec){
         for(int i=0; i<vec.size(); i++){
@@ -44,8 +45,39 @@ public:
         this->left_to_right_inc_peaks = gen_peak_list(true, height);
         this->right_to_left_inc_peaks = gen_peak_list(false, height);
 
-        display(this->left_to_right_inc_peaks);
-        display(this->right_to_left_inc_peaks);
+        multiset<pair<int, int>> multi_set;
+        for(int i=0; i<height.size(); i++){
+            multi_set.insert({height[i], i});
+        }
+
+        while(multi_set.empty()==false){
+            int curr_low_height = (*(multi_set.begin())).first;
+            int curr_low_index = (*(multi_set.begin())).second;
+            multi_set.erase(multi_set.begin());
+
+            int curr_height = curr_low_height;
+            // increasing height until left or right peak doesn't exist
+            while(curr_height>=(left_to_right_inc_peaks[curr_low_index-1]).first  && 
+            curr_height>=(right_to_left_inc_peaks[curr_low_index+1]).first){
+                curr_height += 1;
+            }
+
+            water_level_list.resize(height.size());
+            // left side
+            for(int i=curr_low_index; i!=(left_to_right_inc_peaks[curr_low_index-1]).second; i--){
+                water_level_list[i] = curr_height-curr_low_height;    
+            }
+            // right side
+            for(int i=curr_low_index; i!=(right_to_left_inc_peaks[curr_low_index+1]).second; i--){
+                water_level_list[i] = curr_height-curr_low_height;    
+            }
+        }
+
+
+        int result_sum = 0;
+        for(int i=0; i<water_level_list.size(); i++)
+            result_sum += water_level_list[i];
+        return result_sum;
     }
 };
 
@@ -53,5 +85,5 @@ int main(){
     vector<int> test_vec = {7, 1, 4, 2, 0, 3, 2, 5, 0};
     
     Solution obj;
-    obj.trap(test_vec);
+    cout<<obj.trap(test_vec)<<"\n";
 }
