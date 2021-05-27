9a2df53de4eb42028d00798b469b6b15d37a66f5
Salim <salim7ali@gmail.com>
2021-05-27 21:56:35 +0530

    Created DFS fucntion. Need to modifiy it for the use case.

--- 133. Clone Graph/clone_graph.cpp
+++ 133. Clone Graph/clone_graph.cpp
@@ -1,4 +1,6 @@
-/*
+#include<bits/stdc++.h>
+using namespace std;
+
 // Definition for a Node.
 class Node {
 public:
@@ -17,11 +19,26 @@ public:
         neighbors = _neighbors;
     }
 };
-*/
+
 
 class Solution {
 public:
+    void dfs(Node *node, Node *result, map<int, bool> &visited){
+        visited[node->val] = true;
+
+        for(int i=0; i<node->neighbors.size(); i++){
+            int curr_neighbour = (*(node->neighbors[i])).val;
+            if(visited[curr_neighbour] == false){
+                dfs(node->neighbors[i], result, visited);
+            }
+        }
+    }
+
     Node* cloneGraph(Node* node) {
-        
+        Node *result_node_ptr;
+        map<int, bool> &visited;
+
+        dfs(node, result_node_ptr, visited);
+
     }
 };
