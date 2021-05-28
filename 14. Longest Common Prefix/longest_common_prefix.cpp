b0b85e8569b1485834e8304761e38629f9dbbfdb
Salim <salim7ali@gmail.com>
2021-05-28 22:05:42 +0530

    Added Trie node class.

--- 14. Longest Common Prefix/longest_common_prefix.cpp
+++ 14. Longest Common Prefix/longest_common_prefix.cpp
@@ -1,6 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class TrieNode{
+    char val;
+    int word_count;
+    TrieNode *children[26];
+};
+
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
