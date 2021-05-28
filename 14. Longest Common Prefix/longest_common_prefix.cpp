c29ad098d0dcf010c198e069594a38d19febbddf
Salim <salim7ali@gmail.com>
2021-05-28 22:03:17 +0530

    First commit. Added boiler plate code.

---
+++ 14. Longest Common Prefix/longest_common_prefix.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        
+    }
+};
+
+int main(){
+    vector<string> strs = {"flower", "flow", "flight"};
+    Solution obj;
+    obj.longestCommonPrefix(strs);
+    
+}
