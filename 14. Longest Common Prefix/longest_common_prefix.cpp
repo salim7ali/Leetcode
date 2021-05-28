c34a2dac9c2b235662306efc37e5ad0a0c452d38
Salim <salim7ali@gmail.com>
2021-05-29 00:51:40 +0530

    Code logic completed. Result traversal needs to be added.

--- 14. Longest Common Prefix/longest_common_prefix.cpp
+++ 14. Longest Common Prefix/longest_common_prefix.cpp
@@ -1,21 +1,50 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class TrieNode{
+struct TrieNode{
     char val;
     int word_count;
-    TrieNode *children[26];
+    TrieNode* children[26];
 };
 
 class Solution {
 public:
+    TrieNode* root = new TrieNode();
+
     string longestCommonPrefix(vector<string>& strs) {
-        
+            
+        TrieNode* ptr_node;
+        for(int string_index=0; string_index<strs.size(); string_index++){
+
+            ptr_node = root;
+            for(int j=0; j<strs[string_index].length(); j++){
+                char character = strs[string_index][j];
+                int char_index  = character - 'a';
+                // cout<<char_ascii<<" ";
+
+                if(ptr_node->children[char_index] == NULL){
+                    TrieNode* child_node = new TrieNode();
+                    child_node->val = character;
+                    child_node->word_count += 1;
+                    ptr_node->children[char_index] = child_node;
+
+
+                    ptr_node = ptr_node->children[char_index];
+
+                }else{
+                    ptr_node = ptr_node->children[char_index];
+                    ptr_node->word_count += 1;
+                }
+
+
+            }cout<<"\n";
+        }
     }
 };
 
 int main(){
-    vector<string> strs = {"flower", "flow", "flight"};
+    vector<string> strs = {"apple", "apps", "ape"};
+    // vector<string> strs = {"flower", "flow", "flight"};
     Solution obj;
     obj.longestCommonPrefix(strs);
     
