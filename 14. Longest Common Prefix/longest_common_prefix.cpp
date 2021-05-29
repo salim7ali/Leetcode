78d066c9c4e40048746abae855f18c2118ff113c
Salim <salim7ali@gmail.com>
2021-05-29 17:37:57 +0530

    Added function to find non-NULL address in children.

--- 14. Longest Common Prefix/longest_common_prefix.cpp
+++ 14. Longest Common Prefix/longest_common_prefix.cpp
@@ -4,18 +4,27 @@ using namespace std;
 struct TrieNode{
     char val;
     int word_count;
-    TrieNode* children[26];
+    TrieNode* children[26] = {NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL
+    , NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL};
 };
 
 class Solution {
 public:
     TrieNode* root = new TrieNode();
 
+    TrieNode* findNonNullNode(TrieNode *node){
+        for(int i=0; i<26; i++){
+            if(node->children[i] != NULL)
+                return node->children[i];
+        }
+        return NULL;
+    }
+
     string longestCommonPrefix(vector<string>& strs) {
             
+        // create trie nodes and connections for string chars
         TrieNode* ptr_node;
         for(int string_index=0; string_index<strs.size(); string_index++){
-
             ptr_node = root;
             for(int j=0; j<strs[string_index].length(); j++){
                 char character = strs[string_index][j];
@@ -28,17 +37,32 @@ public:
                     child_node->word_count += 1;
                     ptr_node->children[char_index] = child_node;
 
-
+                    if(ptr_node == root)
+                        root->word_count += 1;
                     ptr_node = ptr_node->children[char_index];
-
                 }else{
                     ptr_node = ptr_node->children[char_index];
                     ptr_node->word_count += 1;
                 }
-
-
-            }cout<<"\n";
+            }
         }
+
+        // Traverse the result
+        string result;
+        if(root->word_count >1 || root->word_count==0)
+            return result;
+        else{
+            ptr_node = findNonNullNode(root);
+            ptr_node = root->children[0];
+            int start_word_count = ptr_node->word_count;
+            while(start_word_count == ptr_node->word_count){
+            
+            }
+            // cout<<"WC="<<root->children[0]->val<<" "<<root->children[0]->word_count;
+        }
+
+
+        
     }
 };
 
