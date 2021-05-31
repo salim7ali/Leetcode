828d5416332aa1e87bc9158b23c69d10706bbd85
Salim <salim7ali@gmail.com>
2021-05-31 13:15:29 +0530

    Completed code logic.

--- 199. Binary Tree Right Side View/right_side_view_of_binary_tree.cpp
+++ 199. Binary Tree Right Side View/right_side_view_of_binary_tree.cpp
@@ -11,8 +11,45 @@ struct TreeNode {
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
  
-class ViewHandler{
+//  RL root
+ 
+class Solution {
+public:
+    void gen_right_side_values(TreeNode* node, int curr_level, vector<int> &right_view_result,vector<bool> &visited_level){
+        if(!node)
+            return;
 
+        if(node->right)
+            gen_right_side_values(node->right, curr_level+1, right_view_result, visited_level);
+        if(node->left)
+            gen_right_side_values(node->left, curr_level+1, right_view_result, visited_level);
+        if(visited_level[curr_level] == false){
+            visited_level[curr_level] = true;
+            right_view_result[curr_level] = node->val;
+        }
+    }
+
+    vector<int> rightSideView(TreeNode* root) {
+        if(root == NULL)
+            return {};
+        
+        vector<int> right_view_result(101);
+        vector<bool> visited_level(101, false);
+        int curr_level = 1;
+
+        gen_right_side_values(root, curr_level, right_view_result, visited_level);
+
+        int no_of_levels=0;
+        for(int i=1; i<visited_level.size(); i++){
+            if(visited_level[i] == false)
+                break;
+            no_of_levels += 1;
+        }
+
+        right_view_result.erase(right_view_result.begin()+no_of_levels+1, right_view_result.end());
+
+        return right_view_result;
+    }
 };
 
 int main(){
