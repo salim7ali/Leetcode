d612fc4d3e1f5bcf26f1fcd78129b39de5ca8d75
Salim <salim7ali@gmail.com>
2021-06-11 22:00:25 +0530

    First commit. Added boiler plate code.

---
+++ 990. Satisfiability of Equality Equations/satisfiability_of_equality_equations.py
@@ -0,0 +1,9 @@
+class Satisfiability:
+    def check_satistiability(equation_list):
+        pass
+
+
+if __name__ == '__main__':
+    equation_list_4 = ["a==b","b!=c","c==a"]
+    obj = Satisfiability()
+    obj.check_satistiability(equation_list_4)
