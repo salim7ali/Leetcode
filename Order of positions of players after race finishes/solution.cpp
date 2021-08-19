50772e4c11d25d6ecdc5fd726fe9e8bbf6ffa476
salim <salim7ali@gmail.com>
2021-08-19 13:01:48 +0530

    First commit. Boiler plate added.

---
+++ Order of positions of players after race finishes/solution.cpp
@@ -0,0 +1,5 @@
+// Given an array A, where A[i] = j denotes the fact that player j finished the race before player i, and for the winner of race let's  say player k, A[k]=-1.  
+// You need to print the order of the positions of players after race finishes
+
+// Follow up: What if the data (array A) got courrupted, how will you take care of that. Return  -1 if there data is courrupted and order of player otherwise.
+
