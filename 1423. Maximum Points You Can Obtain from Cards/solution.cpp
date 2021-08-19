81cdfac25840b90a13824b85826f79c485300bdb
salim <salim7ali@gmail.com>
2021-08-19 16:30:48 +0530

    Optimized further from 52ms to 32ms.

--- 1423. Maximum Points You Can Obtain from Cards/solution.cpp
+++ 1423. Maximum Points You Can Obtain from Cards/solution.cpp
@@ -1,36 +1,42 @@
 // https://leetcode.com/problems/maximum-points-you-can-obtain-from-cards/
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int maxScore(vector<int>& cardPoints, int k) {
-        int currSum=0;
+    int maxScore(vector<int> &cardPoints, int k)
+    {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
+        int currSum = 0;
         int i;
-        for(i=0; i<k; i++){
+        for (i = 0; i < k; i++)
             currSum += cardPoints[i];
-        }
 
         int maxScore = currSum;
         int j;
-        while(i>0){
+        while (i > 0)
+        {
             --i;
-            j = cardPoints.size() + i -k;
-            cout<<i<<" "<<j<<"\n";
+            j = cardPoints.size() + i - k;
+            cout << i << " " << j << "\n";
 
             currSum -= cardPoints[i];
             currSum += cardPoints[j];
-            if(currSum>maxScore)
+            if (currSum > maxScore)
                 maxScore = currSum;
         }
         return maxScore;
     }
 };
 
-int main(){
-    vector<int> cardPoints={1,2,3,4,5,6,1};
-    
+int main()
+{
+    vector<int> cardPoints = {1, 2, 3, 4, 5, 6, 1};
+
     Solution obj1;
-    cout<<obj1.maxScore(cardPoints, 3)<<"\n";
+    cout << obj1.maxScore(cardPoints, 3) << "\n";
 }
