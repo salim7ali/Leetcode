1cc06a049e308e0c5156631af526b57d74b19005
salim <salim7ali@gmail.com>
2021-08-19 16:24:45 +0530

    Solved medium level problem.

--- 1423. Maximum Points You Can Obtain from Cards/solution.cpp
+++ 1423. Maximum Points You Can Obtain from Cards/solution.cpp
@@ -6,7 +6,25 @@ using namespace std;
 class Solution {
 public:
     int maxScore(vector<int>& cardPoints, int k) {
-        
+        int currSum=0;
+        int i;
+        for(i=0; i<k; i++){
+            currSum += cardPoints[i];
+        }
+
+        int maxScore = currSum;
+        int j;
+        while(i>0){
+            --i;
+            j = cardPoints.size() + i -k;
+            cout<<i<<" "<<j<<"\n";
+
+            currSum -= cardPoints[i];
+            currSum += cardPoints[j];
+            if(currSum>maxScore)
+                maxScore = currSum;
+        }
+        return maxScore;
     }
 };
 
@@ -14,5 +32,5 @@ int main(){
     vector<int> cardPoints={1,2,3,4,5,6,1};
     
     Solution obj1;
-    obj1.maxScore(cardPoints, 3);
+    cout<<obj1.maxScore(cardPoints, 3)<<"\n";
 }
