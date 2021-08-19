cd8f23222eca8321bf2b1eb6482af5548aecbdf5
salim <salim7ali@gmail.com>
2021-08-19 16:02:28 +0530

    Frist commit. Added boiler plate code.

---
+++ 1423. Maximum Points You Can Obtain from Cards/solution.cpp
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/maximum-points-you-can-obtain-from-cards/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) {
+        
+    }
+};
+
+int main(){
+    vector<int> cardPoints={1,2,3,4,5,6,1};
+    
+    Solution obj1;
+    obj1.maxScore(cardPoints, 3);
+}
