d7f5ef75da203598a397652f68370d2be0bca4cc
salim <salim7ali@gmail.com>
2021-08-19 13:14:30 +0530

    Solved problem.

--- Order of positions of players after race finishes/solution.cpp
+++ Order of positions of players after race finishes/solution.cpp
@@ -3,3 +3,40 @@
 
 // Follow up: What if the data (array A) got courrupted, how will you take care of that. Return  -1 if there data is courrupted and order of player otherwise.
 
+#include<bits/stdc++.h>
+using namespace std;
+
+class Race{
+    vector<int> before_after_vec;
+    map<int, int> before_after_hm;
+public:
+    Race(vector<int> before_after_vec){
+        this->before_after_vec = before_after_vec;
+    }
+
+    void genOrderOfPositions(){
+        for(int i=0; i<before_after_vec.size(); i++){
+            int before_player_num = before_after_vec[i];
+            int after_player_num = i;
+            before_after_hm[before_player_num] = after_player_num;
+        }
+
+        int curr_player_num = before_after_hm[-1];
+        cout<<curr_player_num<<" ";
+        while (1){
+            if(before_after_hm.find(curr_player_num) == before_after_hm.end())
+                break;
+            curr_player_num = before_after_hm[curr_player_num];
+            cout<<curr_player_num<<" ";
+        }
+    }
+};
+
+int main(){
+    // race results: 2, 3, 0, 4, 1
+    vector<int> before_after_vec = {3, 4, -1, 2, 0};
+    
+    Race obj1(before_after_vec);
+    obj1.genOrderOfPositions();
+
+}
