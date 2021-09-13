c79102d2f937d6db2df99ff0f9486a63e6ca0435
salim <salim7ali@gmail.com>
2021-09-13 19:40:53 +0530

    Added boiler plate code.

---
+++ 199. Max Points on a Line/solution.java
@@ -0,0 +1,7 @@
+// Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane, return the maximum number of points that lie on the same straight line.
+
+class Solution {
+    public int maxPoints(int[][] points) {
+        
+    }
+}
