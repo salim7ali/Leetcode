e1ed44e78576949ba32bffba871ebbdb20202551
salim7ali <salim7ali@gmail.com>
2023-01-29 01:27:43 +0530

    Optimization with call by reference which drops about 15MB of memory and faster runtime as well. Stopping at this since this optimization rabbit hole goes far(use type short, use references for variables, removing comments(depends on how leetcode handles this) , etc)

--- 329. Longest Increasing Path in a Matrix/soln.cpp
+++ 329. Longest Increasing Path in a Matrix/soln.cpp
@@ -11,10 +11,8 @@ public:
         return false;        
     }
     
-    void dfs(vector<vector<int>> &matrix, vector<vector<int>> &dp, int &maxPathLength, int i, int j){
-        //                                  R      B      L      T
-        vector<pair<int, int>> neighbor = {{0,1},{1,0},{0,-1},{-1,0}};
-        bool neighborFound = false;
+    void dfs(vector<vector<int>> &matrix, vector<vector<int>> &dp, int &maxPathLength, int i, int j, vector<pair<int, int>> &neighbor){
+        dp[i][j] = 1;
 
         for(int k=0; k<4; k++){
             int neighRow = i+neighbor[k].first;
@@ -26,32 +24,30 @@ public:
             
             if(dp[neighRow][neighCol] != INT_MIN){ // visited neighbor case
                 dp[i][j] = max(dp[i][j], 1+dp[neighRow][neighCol]);
-                neighborFound = true;   
-            }else if(dp[neighRow][neighCol] == INT_MIN){ //not visited 
-                dfs(matrix, dp, maxPathLength, neighRow, neighCol);
+            }else{ //   dp[neighRow][neighCol] == INT_MIN)  not visited case 
+                dfs(matrix, dp, maxPathLength, neighRow, neighCol, neighbor);
                 // update current as unvisited node/neighbor is resolved for dp
                 dp[i][j] =  max(dp[i][j], 1+dp[neighRow][neighCol]);
-                neighborFound = true;
             }
 
             // update after visiting every neighbor
             maxPathLength = max(maxPathLength, dp[i][j]);
         }
-        if(neighborFound == false)
-            dp[i][j] = 1;
     }
 
     int longestIncreasingPath(vector<vector<int>>& matrix) {
         int ROWS = matrix.size();
         int COLS = matrix[0].size();
         vector<vector<int>> dp(ROWS, vector<int>(COLS, INT_MIN));
+        //                                  R      B      L      T
+        vector<pair<int, int>> neighbor = {{0,1},{1,0},{0,-1},{-1,0}};
 
         int maxPathLength = 1;
         for(int i=0; i<ROWS; i++){
             for(int j=0; j<COLS; j++){
                 // not visited
                 if(dp[i][j] == INT_MIN){
-                    dfs(matrix, dp, maxPathLength, i, j);
+                    dfs(matrix, dp, maxPathLength, i, j, neighbor);
                 }
             }
         }
