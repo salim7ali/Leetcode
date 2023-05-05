d4548de945ea43dbbc2362bd8d1c1c6dc40d98d1
salim7ali <salim7ali@gmail.com>
2023-05-05 22:16:40 +0530

    search for pivot index

--- 33_Search_in_Rotated_Sorted_Array/solution.cpp
+++ 33_Search_in_Rotated_Sorted_Array/solution.cpp
@@ -1,7 +1,33 @@
 // https://leetcode.com/problems/search-in-rotated-sorted-array/
+#include<bits/stdc++.h>
+using namespace std;
 class Solution {
 public:
+    int getPivotIndex(vector<int>& nums){
+        int left = 0;
+        int right = nums.size()-1;
+        int mid = floor((right-left)/2); 
+
+        // no pivot
+        if(nums[left]<nums[mid] && nums[mid]<nums[right])
+            return -1;
+        // has pivot
+        while (left<right && left!=mid)
+        {
+            // check right
+            if(nums[left]<nums[mid] && nums[mid]>nums[right]){
+                left = mid;
+                mid = left + floor((right-left)/2); 
+            // check left 
+            }else if(nums[left]>nums[mid] && nums[mid]<nums[right]){
+                right = mid;
+                mid =  left + floor((right-left)/2); 
+            }
+        }
+        return mid;
+    }
+
     int search(vector<int>& nums, int target) {
-        
+        cout<<getPivotIndex(nums);
     }
 };
