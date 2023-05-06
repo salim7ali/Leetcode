a048ca4f3c3927917f8b6b091fe60f1f93ece804
salim7ali <salim7ali@gmail.com>
2023-05-06 19:23:00 +0530

    SOLVED medium problem. Merged 2 step approach into 1. Easier to understand. Handled else-else edge case.

--- 33_Search_in_Rotated_Sorted_Array/solution.cpp
+++ 33_Search_in_Rotated_Sorted_Array/solution.cpp
@@ -1,56 +1,75 @@
 // https://leetcode.com/problems/search-in-rotated-sorted-array/
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class Solution {
+class Solution
+{
 public:
-    int getPivotIndex(vector<int>& nums){
+    int findIndex(vector<int> &nums, int target)
+    {
         int left = 0;
-        int right = nums.size()-1;
-        int mid = floor((right-left)/2); 
+        int right = nums.size() - 1;
+        int mid;
 
         // has pivot
-        while (left<right)
+        while (left <= right)
         {
-            if(nums[left]<nums[mid] && nums[mid]<nums[right])
-                return right;
-            // pivot found
-            if(nums[mid-1]<nums[mid] && nums[mid]>nums[mid+1])
-                return mid;
-            // check right
-            if(nums[left]<nums[mid] && nums[mid]>nums[right]){
-                left = mid+1;
-                mid = left + floor((right-left)/2); 
-            // check left 
-            }else if(nums[left]>nums[mid] && nums[mid]<nums[right]){
-                right = mid-1;
-                mid =  left + floor((right-left)/2); 
-            }
-        }
-        return mid;
-    }
+            mid = left + floor((right - left) / 2);
 
-    int myBinarySearchRetIndex(vector<int> &nums, int element, int l, int r){
-
-        while (l<=r)
-        {
-            int mid = l+floor((r-l)/2);
-            if(nums[mid]==element)
+            if(nums[mid]==target)
                 return mid;
-            else if(element<nums[mid]){
-                r = mid-1;
-            }else{
-                l = mid+1;
+            //  left side increasing
+            if(nums[left]<=nums[mid]){
+                //  element lies inbetween it or not
+                if( nums[left]<=target && target<=nums[mid])
+                    right = mid-1;
+                else
+                    left = mid+1;
+            }else{  // right side increasing
+                //  element lies inbetween it or not
+                if(nums[mid]<=target && target<=nums[right])
+                    left = mid+1;
+                else
+                    right = mid-1;
             }
         }
         return -1;
     }
 
-    int search(vector<int>& nums, int target) {
-        int pivot = getPivotIndex(nums);
-        
-        if((nums[0]<=target) && (target<=nums[pivot]))
-            return myBinarySearchRetIndex(nums, target, 0, pivot);
-        else
-            return myBinarySearchRetIndex(nums, target, pivot+1, nums.size()-1);
+    int myBinarySearchRetIndex(vector<int> &nums, int element, int l, int r)
+    {
+
+        while (l <= r)
+        {
+            int mid = l + floor((r - l) / 2);
+            if (nums[mid] == element)
+                return mid;
+            else if (element < nums[mid])
+            {
+                r = mid - 1;
+            }
+            else
+            {
+                l = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    int search(vector<int> &nums, int target)
+    {
+        return findIndex(nums, target);
+
+        // if ((nums[0] <= target) && (target <= nums[pivot]))
+        //     return myBinarySearchRetIndex(nums, target, 0, pivot);
+        // else
+        //     return myBinarySearchRetIndex(nums, target, pivot + 1, nums.size() - 1);
     }
 };
+
+void main()
+{
+    vector<int> nums = {3, 1};
+    Solution obj;
+
+    obj.search(nums, 0);
+}
