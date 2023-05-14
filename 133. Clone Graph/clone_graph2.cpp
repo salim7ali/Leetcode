c2dfc87ed4cafa930916c695345c95a44327cf1c
salim7ali <salim7ali@gmail.com>
2023-05-15 00:19:18 +0530

    solved but test case not passing where graph doesn't have any nodes

--- 133. Clone Graph/clone_graph2.cpp
+++ 133. Clone Graph/clone_graph2.cpp
@@ -1,4 +1,6 @@
-/*
+#include<bits/stdc++.h>
+using namespace std;
+
 // Definition for a Node.
 class Node {
 public:
@@ -17,11 +19,34 @@ public:
         neighbors = _neighbors;
     }
 };
-*/
+
 
 class Solution {
 public:
-    Node* cloneGraph(Node* node) {
+    Node* graphCloner(Node* currOrigNode, map<Node*, Node*> &oldToNewMap){
         
+        Node* currDupNode = new Node(currOrigNode->val);
+        oldToNewMap[currOrigNode] = currDupNode;
+
+        Node* currOrigNeighbor;
+        Node* currDupNeighbor;
+        for(int i=0; i<currOrigNode->neighbors.size(); i++){
+            currOrigNeighbor = currOrigNode->neighbors[i];   
+            
+            if(oldToNewMap.find(currOrigNeighbor) == oldToNewMap.end()){ // not visited
+                currDupNode->neighbors.push_back(graphCloner(currOrigNeighbor, oldToNewMap));
+            }else{
+                currDupNeighbor = oldToNewMap[currOrigNeighbor];
+                currDupNode->neighbors.push_back(currDupNeighbor);
+            }
+        }
+        return currDupNode;
+    }
+
+    Node* cloneGraph(Node* node) {
+        map<Node*, Node*> oldToNewMap;
+        
+        return graphCloner(node, oldToNewMap);
+
     }
 };
