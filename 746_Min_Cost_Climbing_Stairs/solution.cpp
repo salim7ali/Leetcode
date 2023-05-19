e3ffd5d15223dbd63bbd91e3dfbc609666e5f24e
salim7ali <salim7ali@gmail.com>
2023-05-20 00:07:11 +0530

    Solved but with bugs

--- 746_Min_Cost_Climbing_Stairs/solution.cpp
+++ 746_Min_Cost_Climbing_Stairs/solution.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
+    void minCostFind(vector<int> &cost, vector<int> &dp, int currIndex){
+        if(currIndex ==0 || currIndex ==1){
+            dp[currIndex] = cost[currIndex];
+            return;
+        } 
+        dp[currIndex] = min(cost[currIndex]+dp[currIndex-1], cost[currIndex]+dp[currIndex-2]);
+    }
+
     int minCostClimbingStairs(vector<int>& cost) {
-        
+        vector<int> dp(cost.size()+1);
+
+        minCostFind(cost, dp, cost.size());
+        return dp[cost.size()];
+
     }
 };
