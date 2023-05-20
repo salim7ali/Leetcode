abc03ad5bfe5fcb9f70058b18c6064b69c2cea51
salim7ali <salim7ali@gmail.com>
2023-05-21 00:54:40 +0530

    Solved

--- 746_Min_Cost_Climbing_Stairs/solution.cpp
+++ 746_Min_Cost_Climbing_Stairs/solution.cpp
@@ -1,19 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
-    void minCostFind(vector<int> &cost, vector<int> &dp, int currIndex){
-        // needs fixing
-        if(currIndex ==0 || currIndex ==1){
-            dp[currIndex] = cost[currIndex];
-            return;
-        } 
-        dp[currIndex] = min(cost[currIndex]+dp[currIndex-1], cost[currIndex]+dp[currIndex-2]);
+    int minCostFind(vector<int> &cost, vector<int> &dp, int currIndex){
+        int prevIndex = currIndex-1;
+        int prevPrevIndex = currIndex-2;
+        
+        if(dp[prevIndex] == INT_MIN ){
+            dp[prevIndex] = minCostFind(cost, dp, prevIndex);
+        }
+        if(dp[prevPrevIndex] == INT_MIN ){
+            dp[prevPrevIndex] = minCostFind(cost, dp, prevPrevIndex);
+        }
+
+        if(currIndex == cost.size())
+            return min(dp[prevIndex], dp[prevPrevIndex]);    
+
+        return min(cost[currIndex]+dp[prevIndex], cost[currIndex]+dp[prevPrevIndex]);
     }
 
     int minCostClimbingStairs(vector<int>& cost) {
-        vector<int> dp(cost.size()+1);
+        vector<int> dp(cost.size()+1, INT_MIN);
+        dp[0] = cost[0];
+        dp[1] = cost[1];
 
-        minCostFind(cost, dp, cost.size());
-        return dp[cost.size()];
+        int minCost = minCostFind(cost, dp, cost.size());
+
+        for(int i=0; i<dp.size(); i++){
+            cout<<dp[i]<<" ";
+        }cout<<"\n";
+
+        return minCost;
+        // return dp[cost.size()];
 
     }
 };
